8f9ffe0c4c65aa080b2177e998c45ed4b31ce0bd
ToastFreak777 <gmcdonald604@gmail.com>
2025-06-19 12:17:25 -0400

    Initial commit

---
+++ frontend_react/src/constants/index.js
@@ -0,0 +1,15 @@
+import images from './images';
+
+// const navBarLinks = [ 'home', 'about', 'work', 'skills', 'contact' ]
+
+const navBarLinks = [
+  'home', 'work', 'skills', 'contact'
+]
+
+// const tags = [ 'UI/UX', 'Web App', 'Mobile App', 'React JS', 'All' ]
+
+const tags = [
+  'All', 'Web', 'Mobile', 'API'
+]
+
+export { images, navBarLinks, tags };

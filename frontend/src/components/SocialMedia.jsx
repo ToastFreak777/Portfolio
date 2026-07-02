dc480e8b99a46b75062ed863211b7080020e780b
ToastFreak777 <gmcdonald604@gmail.com>
2026-07-01 20:12:25 -0400

    swapped links, removed mobile filter

--- frontend/src/components/SocialMedia.jsx
+++ frontend/src/components/SocialMedia.jsx
@@ -1,17 +1,28 @@
-import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
+import React from "react";
+import { links } from "../constants";
+
+// import { BsTwitter, BsInstagram } from 'react-icons/bs';
+import { BsGithub, BsLinkedin } from "react-icons/bs";
+// import { FaFacebookF } from "react-icons/fa";
 
 const SocialMedia = () => (
   <div className="app__social">
-    <div>
-      <BsTwitter />
+    <div
+      onClick={() => {
+        window.open(links.github, "_blank");
+      }}
+    >
+      <BsGithub />
     </div>
-    <div>
+    {/* <div>
       <FaFacebookF />
-    </div>
-    <div>
-      <BsInstagram />
+    </div> */}
+    <div
+      onClick={() => {
+        window.open(links.linkedin, "_blank");
+      }}
+    >
+      <BsLinkedin />
     </div>
   </div>
 );
--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,15 +1,17 @@
-import images from './images';
+import images from "./images";
 
 // const navBarLinks = [ 'home', 'about', 'work', 'skills', 'contact' ]
 
-const navBarLinks = [
-  'home', 'work', 'skills', 'contact'
-]
+const navBarLinks = ["home", "work", "skills", "contact"];
 
 // const tags = [ 'UI/UX', 'Web App', 'Mobile App', 'React JS', 'All' ]
 
-const tags = [
-  'All', 'Web', 'Mobile', 'API'
-]
+// const tags = ["All", "Web", "Mobile", "API"];
+const tags = ["All", "Web", "API"];
 
-export { images, navBarLinks, tags };
+const links = {
+  github: "https://github.com/ToastFreak777",
+  linkedin: "https://linkedin.com/in/goodly-mcdonald-499145251",
+};
+
+export { images, navBarLinks, tags, links };
